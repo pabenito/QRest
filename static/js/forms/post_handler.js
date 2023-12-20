75c0535ca7e3d877f4c2e4193d9a60a96542fa64
pabenito <pedrobenitowork@gmail.com>
2023-12-20 10:51:00 +0100

    refactor confirm to post_hadler

--- static/js/forms/post_handler.js
+++ static/js/forms/post_handler.js
@@ -1,15 +1,18 @@
-function confirm(url_post, url_redirect) {
+import {showError} from "../utils/errorHandler";
+function post_handler(url_post, url_redirect) {
     // Enviar la solicitud POST vacía mediante fetch
     fetch(url_post, {
         method: 'POST'
     }).then(response => {
         if (!response.ok) {
-            console.log(`Error al confirmar comanda: ${response.status} ${response.text()}`);
+            console.log(`Error: ${response.status} ${response.text()}`);
+            showError(`Error: ${response.status} ${response.text()}`);
         }
     }).then(data => {
         // Aquí puedes redirigir o manejar la respuesta como necesites
         window.location.href = url_redirect;
     }).catch(error => {
         console.error('Error:', error);
+        showError(`Error: ${error}`);
     });
 }
